669cf0364873859aa0c66ff1477385234b4751a8
sourabh-kashyap <39377525+sourabh-kashyap@users.noreply.github.com>
2018-05-21 23:26:10 +0530

    Update myfirstprogram.py

--- myfirstprogram.py
+++ myfirstprogram.py
@@ -1,30 +1,15 @@
-# This program can be used to ask user Name and Age and will analyse the length of your name and your age in a year.
+# This program can be used to ask user Name and Age, It will analyse the length of the name (in characters) and will show the age in a year.
 
 print ("Hello, My name is Mr. Program!!!")
-
 print ('')
-
 print ("What's your name?")
-
 print ('')
-
 myName = input()
-
 print ('')
-
 print ('welcome "' + myName + '" !!! ' + 'Your name has "' + str (len(myName)) + '" character.')
-
 print ('')
-
 print ("What is your age?")
-
 print ('')
-
 myAge = input()
-
 print ('')
-
 print ('You will be "' + str(int(myAge) + 1) + '" in a year.')
-
-
-
